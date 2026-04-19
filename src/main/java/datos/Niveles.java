3935dc0c47243b4dc82064553c00d601e0361605
CristianDiaA <cristiand-diaza@unilibre.edu.co>
2026-04-19 05:07:47 +0000

    Atributos

--- src/main/java/datos/Niveles.java
+++ src/main/java/datos/Niveles.java
@@ -1,4 +1,12 @@
-
 package datos;
+import lombok.Getter;
+@Getter
+
 public class Niveles {
+
+    private int deuda;
+
+    public Niveles(int nivel){
+        deuda=nivel*5000;
+    }
 }
--- src/main/java/datos/ValoresJuego.java
+++ src/main/java/datos/ValoresJuego.java
@@ -1,5 +1,10 @@
 package datos;
-
+import lombok.Getter;
+@Getter
 public class ValoresJuego {
+    private double girasol=1.5;
+    private double hongo=1;
+    private double michael=-0.3;
+    private double faraon=-0.15;
 
 }
